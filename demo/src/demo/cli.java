5d55d5b5c754c9f3af7e7116d5659c6c9114011a
lbro23 <leonbreaux23@gmail.com>
2016-05-06 15:00:03 -0400

    Push

--- demo/src/demo/cli.java
+++ demo/src/demo/cli.java
@@ -5,23 +5,33 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class cli extends Thread {
+	Socket sock;
+	Scanner sc;
+	Scanner sc1;
 	
 	public static void main(String[] args) {
 		cli c = new cli();
 		c.start();
 	}
+	
+	public cli() {
+		try { 
+			sc = new Scanner(System.in);
+			sock = new Socket("10.20.0.231", 4445);
+			sc1 = new Scanner(sock.getInputStream());
+		} catch (Exception e) {
+			
+		}
+	}
 
 	public void run() {
 		try{
-		Scanner sc = new Scanner(System.in);
-		Socket sock = new Socket("10.20.0.231", 4445);
-		Scanner sc1 = new Scanner(sock.getInputStream());
-		System.out.println("type any number");
-		int number = sc.nextInt();
-		PrintStream p = new PrintStream(sock.getOutputStream());
-		
-		p.println(number);
-		System.out.println(sc1.nextInt());
+			System.out.println("type any number");
+			int number = sc.nextInt();
+			PrintStream p = new PrintStream(sock.getOutputStream());
+			
+			p.println(number);
+			System.out.println(sc1.nextInt());
 		
 		}catch (Exception e){
 			e.printStackTrace();
