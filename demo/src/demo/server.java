1a988c4e2a7689b139141cdc9cb1678fda8408a2
jakemonty <jakemontgomery17@marist.com>
2016-05-06 14:59:41 -0400

    Updates

--- demo/src/demo/server.java
+++ demo/src/demo/server.java
@@ -6,27 +6,44 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class server extends Thread {
-	
+	private ServerSocket sock;
+	Socket ss;
+	Scanner sc;
+
+		
+	public server() {
+		try {
+			sock = new ServerSocket(4445);
+			ss = sock.accept();
+			sc = new Scanner(ss.getInputStream());
+			this.start();
+		}catch (Exception e) {
+			
+		}
+	}
 
 	public static void main(String[] args){
 		server s = new server();
-		s.start();
 	}
+	
 	public void run(){
 		try{
-			ServerSocket sock = new ServerSocket(4445);
-			Socket ss = sock.accept();
-			Scanner sc = new Scanner(ss.getInputStream());
-			int number = sc.nextInt();
+//			ss = sock.accept();
+//			sc = new Scanner(ss.getInputStream());
+			while(true) {
+				if(sc.hasNext()) {
+					int number = sc.nextInt();
+					
+					number*=3;
+					System.out.println(number);
+					
+					PrintStream p = new PrintStream(ss.getOutputStream());
+					p.println(number);
+				}
+			}				
+				}catch(Exception e){
+					e.printStackTrace();
+				}
 			
-			number*=3;
-			System.out.println(number);
-			
-			PrintStream p = new PrintStream(ss.getOutputStream());
-			p.println(number);
-			
-			}catch(Exception e){
-				e.printStackTrace();
-			}
 	}
 }
