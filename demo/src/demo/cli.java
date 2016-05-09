f3ef27c6342cfe1873b4df4100eaa78c9c1d5e91
jakemonty <jakemontgomery17@marist.com>
2016-05-09 11:32:06 -0400

    fixes

--- demo/src/demo/cli.java
+++ demo/src/demo/cli.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class cli extends Thread {
 	Socket sock;
 	Scanner sc;
-	Scanner sc1;
 	
 	public static void main(String[] args) {
 		cli c = new cli();
@@ -18,7 +17,7 @@ public class cli extends Thread {
 		try { 
 			sc = new Scanner(System.in);
 			sock = new Socket("10.20.0.231", 4445);
-			sc1 = new Scanner(sock.getInputStream());
+			
 		} catch (Exception e) {
 			
 		}
@@ -26,6 +25,7 @@ public class cli extends Thread {
 
 	public void run() {
 		try{
+			Scanner sc1 = new Scanner(sock.getInputStream());
 			System.out.println("type any number");
 			int number = sc.nextInt();
 			PrintStream p = new PrintStream(sock.getOutputStream());
--- demo/src/demo/server.java
+++ demo/src/demo/server.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class server extends Thread {
 	private ServerSocket sock;
 	Socket ss;
-	Scanner sc;
+
 
 		
 	public server() {
 		try {
 			sock = new ServerSocket(4445);
 			ss = sock.accept();
-			sc = new Scanner(ss.getInputStream());
+			
 			this.start();
 		}catch (Exception e) {
 			
@@ -28,10 +28,9 @@ public class server extends Thread {
 	
 	public void run(){
 		try{
-//			ss = sock.accept();
-//			sc = new Scanner(ss.getInputStream());
-			while(true) {
-				if(sc.hasNext()) {
+			
+			Scanner sc = new Scanner(ss.getInputStream());
+			
 					int number = sc.nextInt();
 					
 					number*=3;
@@ -39,8 +38,8 @@ public class server extends Thread {
 					
 					PrintStream p = new PrintStream(ss.getOutputStream());
 					p.println(number);
-				}
-			}				
+				
+							
 				}catch(Exception e){
 					e.printStackTrace();
 				}
