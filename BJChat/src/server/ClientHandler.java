e992aba1e996a7e46fc34643efb80a17ad7b684c
lbro23 <leonbreaux23@gmail.com>
2016-05-11 21:57:34 -0400

    Server Groundwork BJChat

--- BJChat/src/server/ClientHandler.java
+++ BJChat/src/server/ClientHandler.java
@@ -1,5 +1,50 @@
 package server;
+/**
+ * This class will listen to the input stream from this client and be responsible for
+ * soliciting a response from the server to this client
+ * @author Leon
+ *
+ */
+public class ClientHandler implements Runnable {
+	User user;
+	Server server;
+	
+	boolean running; // used to stop infinite loop
+	
+	public ClientHandler(Server server, User user) {
+		this.user = user;
+		this.server = server;
+	}
 
-public class ClientHandler {
+	/**
+	 * Watch the input from this client, and handle
+	 * it accordingly
+	 */
+	@Override
+	public void run() {
+		while(running) {
+			if(user.getInputStream().hasNext()) {
+				// TODO Message received from client
+			}
+		}
+	}
+	
+	/**
+	 * Prints this message to the client's output stream
+	 * @param message - message to be printed
+	 */
+	public void sayToClient(String message) {
+		user.getOutputStream().println(message);
+	}
+	
+	/**
+	 * Closes this client, and all associated connections
+	 */
+	public void close() {
+		running = false;
+		user.close();
+	}
+	
+	
 
 }
--- BJChat/src/server/User.java
+++ BJChat/src/server/User.java
@@ -1,36 +1,73 @@
 package server;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class User {
+	static ArrayList<User> allUsers;
+	
 	Socket socket;
 	String name;
 	int id;
 	
-	PrintStream outputStream;
-	InputStream inputStream;
+	PrintStream outputToClient;
+	Scanner inputFromClient;
 	
 	
 	public User(Socket socket, String name, int id) {
+		// first time initialization
+		if(allUsers == null) {
+			allUsers = new ArrayList<User>();
+		}
 		this.socket = socket;
 		this.name = name;
 		this.id = id;
 
 		try {
-			outputStream = new PrintStream(socket.getOutputStream());
-			inputStream = socket.getInputStream();
+			outputToClient = new PrintStream(socket.getOutputStream());
+			inputFromClient = new Scanner(socket.getInputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		allUsers.add(this);
 	}
 	
-	public PrintStream getOutputStream() { return outputStream; }
-	public InputStream getInputStream() { return inputStream; }
+	/**
+	 * close this user and all associated connections
+	 */
+	public void close() {
+		allUsers.remove(this);
+		try {
+			outputToClient.close();
+			inputFromClient.close();
+			socket.close();
+		} catch(Exception e) { e.printStackTrace(); }
+	}
 	
-	public String 
+	public PrintStream getOutputStream() { return outputToClient; }
+	public Scanner getInputStream() { return inputFromClient; }
+	
+	public String getName() { return name; }
+	public int getID() { return id; }
+	public Socket getSocket() { return socket; }
+	
+	/**
+	 * Find the user with the given id
+	 * @return - the user with the given id
+	 * 			or null if no such user exists
+	 */
+	public static User findUser(int id) {
+		for(User u: allUsers) {
+			if(u.getID() == id) {
+				return u;
+			}
+		}
+		return null;
+	}
 	
 	
 }
