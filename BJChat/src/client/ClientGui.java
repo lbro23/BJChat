f9cb085e49987eda85f4e958b7bd6e3305b93518
jakemonty <jakemontgomery17@marist.com>
2016-05-12 09:28:44 -0400

    I Suck

--- BJChat/src/client/ClientGui.java
+++ BJChat/src/client/ClientGui.java
@@ -5,7 +5,17 @@ import javax.swing.JFrame;
 public class ClientGui extends JFrame {
 	
 	public ClientGui(){
+		super("BJChat");
+		setupGui();
+	}
+
+	private void setupGui() {
 		
+		
+	}
+	
+	public void setName(String name){
+		this.setTitle(name + "'s BJChat");
 	}
 
 }
