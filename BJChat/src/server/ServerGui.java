a03f673e41c54dff8f842583b2b1511b2032b091
lbro23 <leonbreaux23@gmail.com>
2016-05-12 11:01:37 -0400

    Server GUI

--- BJChat/src/server/ServerGui.java
+++ BJChat/src/server/ServerGui.java
@@ -1,11 +1,15 @@
 package server;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -13,42 +17,73 @@ import javax.swing.JTextPane;
 public class ServerGui extends JFrame {
 	JTextField input;
 	JTextPane console;
+	JTextPane users;
 	JButton button;
 	
 	public ServerGui() {
 		super("BJ Chat Server");
 		this.setLocation(300, 100);
-		this.setSize(400, 600);
-		this.setVisible(true);
+		this.setSize(700, 600);
 		this.addWindowListener(createWindowListener());
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setupGui();
+		this.setVisible(true);
 	}
 	
 	public void setupGui() {
 		input = new JTextField();
 		console = new JTextPane();
+		users = new JTextPane();
 		button = new JButton("Send");
+		
+		console.setEditable(false);
+		console.setPreferredSize(new Dimension(100, this.getHeight() - 100));
+		console.setBackground(Color.WHITE);
+		
+		users.setEditable(false);
+		users.setPreferredSize(new Dimension(50, this.getHeight() - 100));
+		users.setBackground(Color.GRAY);
 		
 		Box vertical = Box.createVerticalBox();
 		Box bottom = Box.createHorizontalBox();
+		Box whole = Box.createHorizontalBox();
+		Box top = Box.createHorizontalBox();
+		Box stream = Box.createVerticalBox();
+		Box clients = Box.createVerticalBox();
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.add(input);
+		JScrollPane consolePane = new JScrollPane(console, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		JScrollPane userPane = new JScrollPane(users, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		
-		vertical.add(scrollPane);
+		stream.add(new JLabel("Server Console"));
+		stream.add(Box.createVerticalStrut(10));
+		stream.add(consolePane);
+		
+		clients.add(new JLabel("Connected Users"));
+		clients.add(Box.createVerticalStrut(10));
+		clients.add(userPane);
+		
+		top.add(stream);
+		top.add(Box.createHorizontalStrut(10));
+		top.add(clients);
+		
+		vertical.add(Box.createVerticalStrut(20));
+		vertical.add(top);
 		vertical.add(Box.createVerticalStrut(10));
 		vertical.add(bottom);
-		
-		console.setText("HELLO WORLD");
-		console.setSize(200, 200);
+		vertical.add(Box.createVerticalStrut(10));
 		
 		bottom.add(input);
 		bottom.add(Box.createHorizontalStrut(10));
 		bottom.add(button);
 		
-		this.add(vertical);
+		
+		whole.add(Box.createHorizontalStrut(10));
+		whole.add(vertical);
+		whole.add(Box.createHorizontalStrut(10));
+		
+		this.add(whole);
 	}
 	
 	public static void main(String[] args) {
