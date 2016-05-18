c1925074fd2293cb938e9db55410138dedf2d8b7
lbro23 <leonbreaux23@gmail.com>
2016-05-18 10:02:30 -0400

    DM Update

--- BJChat/src/client/DirectMessageWindow.java
+++ BJChat/src/client/DirectMessageWindow.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -10,6 +11,7 @@ import java.awt.event.WindowListener;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -19,6 +21,7 @@ public class DirectMessageWindow extends JFrame implements ActionListener, KeyLi
 	JTextPane text;
 	JTextField input;
 	JButton button;
+	JScrollBar scrollBar;
 	
 	public DirectMessageWindow(String name, Client c) {
 		super("BJ Chat DM with " + name);
@@ -32,15 +35,46 @@ public class DirectMessageWindow extends JFrame implements ActionListener, KeyLi
 	}
 
 	private void setupGui() {
+		button = new JButton("Send");
+		
+		text = new JTextPane();
+		text.setEditable(false);
+		text.setPreferredSize(new Dimension(100, 300));
+		
+		input = new JTextField();
+		text.setEditable(true);
+		
 		Box whole = Box.createVerticalBox();
 		Box bottom = Box.createHorizontalBox();
 		
 		JScrollPane consolePane = new JScrollPane(text, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollBar = consolePane.getVerticalScrollBar();
+		
+		bottom.add(input);
+		bottom.add(Box.createHorizontalStrut(10));
+		bottom.add(button);
 		
 		whole.add(Box.createVerticalStrut(10));
 		whole.add(consolePane);
+		whole.add(Box.createVerticalStrut(10));
+		whole.add(bottom);
+		whole.add(Box.createVerticalStrut(10));
+		
+		this.add(whole);
 	}
+	
+	public void println(String message) {
+		text.setText(text.getText() + message + "\n");
+		scrollDown();
+	}
+	
+	private void scrollDown() {
+		if(scrollBar != null) {
+			scrollBar.setValue(scrollBar.getMaximum());
+		}
+	}
+	
 	///////////////// LISTENER METHODS //////////////////
 	// Key Listener
 	@Override
