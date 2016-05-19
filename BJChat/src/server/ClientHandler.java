fc738402d295fbad7aa60fc28bda6ad273c7273a
lbro23 <leonbreaux23@gmail.com>
2016-05-19 13:13:35 -0400

    We Dun Mesed Up

--- BJChat/src/server/ClientHandler.java
+++ BJChat/src/server/ClientHandler.java
@@ -39,7 +39,8 @@ public class ClientHandler implements Runnable {
 				} else {
 					server.sayToAllClients(message);
 				}
-			} // hi
+				lastConnectTime = System.currentTimeMillis();
+			}
 		} catch (NoSuchElementException e) {}
 	}
 	
