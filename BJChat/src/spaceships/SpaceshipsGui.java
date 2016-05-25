9119608a3681a49d5e463a951e7e5c64733c60fd
lbro23 <leonbreaux23@gmail.com>
2016-05-25 11:56:11 -0400

    Stuff

--- BJChat/src/spaceships/SpaceshipsGui.java
+++ BJChat/src/spaceships/SpaceshipsGui.java
@@ -1,8 +1,14 @@
 package spaceships;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SpaceshipsGui extends JFrame {
+	JPanel canvas;
 	
 	public SpaceshipsGui() {
 		super("BM Spaceships");
@@ -12,7 +18,25 @@ public class SpaceshipsGui extends JFrame {
 	}
 	
 	public void setupGui() {
+		canvas = new JPanel();
+		canvas.setPreferredSize(new Dimension(500, 500));
+		canvas.setBackground(Color.BLACK);
+		Box wholeH = Box.createHorizontalBox();
+		Box wholeV = Box.createVerticalBox();
 		
+		wholeV.add(Box.createVerticalStrut(10));
+		wholeV.add(canvas);
+		wholeV.add(Box.createVerticalStrut(10));
+		
+		wholeH.add(Box.createHorizontalStrut(10));
+		wholeH.add(wholeV);
+		wholeH.add(Box.createHorizontalStrut(10));
+		
+		this.add(wholeH);
+	}
+	
+	public void drawGame(GameState state) {
+		// TODO Draw Game from game state
 	}
 	
 	public static void main(String[] args) {
